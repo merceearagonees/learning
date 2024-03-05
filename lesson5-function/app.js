20d445852e112ef9dcf7aff713e48ea2894addf4
merceearagonees <merceearagonees@gmail.com>
2024-03-05 01:03:13 +0100

    lesson4

---
+++ lesson5-function/app.js
@@ -0,0 +1,68 @@
+function bottleOpener (bottleType) {
+    
+    const bottleTypes = ['beer', 'wine'];
+    if (bottleType === 'beer bottle' || bottleType === 'wine bottle') {
+        console.log('open bottles ' + bottleType);
+
+    }
+    else {
+        console.log('invalid bottleType');
+    }
+
+}
+
+bottleOpener('beer bottle');
+bottleOpener('wine bottle');
+bottleOpener('coke bottle');
+
+// const bottleTypes = ['beer', 'wine'];
+// const hasValidType = bottleTypes.includes('coke');
+// console.log(hasValidType);
+
+
+// ? igual a if
+// : igual a else
+
+function iceCreamMachine (flavor, size, topping) {
+    console.log(flavor);
+    console.log(size);
+    console.log(topping);
+
+
+}
+
+iceCreamMachine('chocolate', 'big', 'nuts')
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+const sum = add(1, 3)
+console.log(sum);
+
+function vendingMachine(productType, productName) {
+    if (productType === 'drink') {
+        getDrink(productName);
+
+    } else if(productType === 'chocolate') {
+        getChocolate(productName);
+    } else {
+        console.log('invalid product name');
+    }
+}
+
+function getDrink(name) {
+    console.log('drink:; ' + name);
+}
+
+function getChocolate(name) {
+    console.log('chocolate:; ' + name);
+}
+
+vendingMachine('drink', 'locozade');
+vendingMachine('chocolate', 'hazelnut');
+
+
+
+
